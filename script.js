3f4bde128c5cbe5115279c359c7f315d151dfee5
Geronimo Amuchastegui <gamuchastegui@MENDOZACIUDAD.MZA>
2024-09-11 13:31:06 -0300

    se sigue trabajar en fisca

--- script.js
+++ script.js
@@ -61,3 +61,25 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     carouselElement.addEventListener('slid.bs.carousel', updateText)
 })
+
+//funcion cambio de mapa inspectores
+
+function changeMap(inspectorId){
+
+    const maps = document.querySelectorAll('fisca--div-flex-img')
+
+    maps.forEach(map=> {
+
+        map.classList.remove('display-block')
+        map.classList.add('display-none')
+
+    })
+
+    const selectedMap = document.getElementById(inspectorId)
+
+        selectedMap.classList.remove('display-none')
+        
+        selectedMap.classList.add('display-block')
+
+    }
+
