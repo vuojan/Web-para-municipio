3dbaa12ed671eaf5a5ef39752a678ec68d35ed54
Geronimo Amuchastegui <gamuchastegui@MENDOZACIUDAD.MZA>
2024-09-18 13:27:47 -0300

    Se sigue trabajando en fiscalizacion

--- script.js
+++ script.js
@@ -64,22 +64,5 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 //funcion cambio de mapa inspectores
 
-function changeMap(inspectorId){
 
-    const maps = document.querySelectorAll('fisca--div-flex-img')
-
-    maps.forEach(map=> {
-
-        map.classList.remove('display-block')
-        map.classList.add('display-none')
-
-    })
-
-    const selectedMap = document.getElementById(inspectorId)
-
-        selectedMap.classList.remove('display-none')
-        
-        selectedMap.classList.add('display-block')
-
-    }
 
